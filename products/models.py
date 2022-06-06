3fc1e0df61549f308a266536b19e094b3af26051
KOCEAN <kocean@KOCEANui-MacBookPro.local>
2022-06-07 01:04:39 +0900

    ADD: databases

--- products/models.py
+++ products/models.py
@@ -1,3 +1,39 @@
+from itertools import product
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Menu(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'menus'
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'categories'
+
+class Product(models.Model):
+    name     = models.CharField(max_length=100)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'products'
+
+class Allergy(models.Model):
+    name     = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = 'allergies'
+
+class AllergyDrink(models.Model):
+    name     = models.CharField(max_length=20)
+    allergy = models.ForeignKey('Allergy', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'allergydrinks'
